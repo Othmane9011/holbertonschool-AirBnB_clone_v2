80bfdadbea2971cc303d47fa29aadb7a94ad4cdf
Othmane9011 <5650@holbertonstudents.com>
2023-08-03 22:29:15 +0200

    task1

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,17 +114,49 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
+        """ Create an object of any class"""       
+        new_args = args.split(" ")
+        if not new_args[0]:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif new_args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[new_args[0]]()
+        for arg in new_args[1:]:
+            arg_split = arg.split('=')
+            if len(arg_split) < 2:
+                continue
+            
+            arg_name  = arg_split[0]
+            arg_value = arg_split[1]
+            
+            value = None
+            if '.' in arg_value and arg_value.replace('.', '').isdigit():
+                value = float(arg_value)
+            elif arg_value.isdigit():
+                value = int(arg_value)
+            else:
+                value = arg_value.replace('\"', '').replace('_', ' ')
+            
+            setattr(new_instance, arg_name, value)
+            
+        # for i in range(1, len(new_args)):
+        #     nre_args = new_args[i].split("=")
+        #     try:
+        #         if nre_args[1][0] == "\"":
+        #             nre_args[1] = nre_args[1].replace("\"","")
+        #             nre_args[1] = nre_args[1].replace("_"," ")
+        #         elif nre_args[1].isdigit() and "." in nre_args[1]:
+        #             nre_args[1] = float(nre_args[1])
+        #         else:
+        #             nre_args[1] = int(nre_args[1])
+        
+        #     except:
+        #         pass
+        #     setattr(new_instance,nre_args[0],nre_args[1])                              
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -187,7 +219,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del(storage.__objects[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -259,7 +291,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         # determine if key is present
-        if key not in storage.all():
+        if key not in storage.__objects:
             print("** no instance found **")
             return
 
@@ -272,7 +304,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +312,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -293,7 +325,7 @@ class HBNBCommand(cmd.Cmd):
             args = [att_name, att_val]
 
         # retrieve dictionary of current objects
-        new_dict = storage.all()[key]
+        new_dict = storage.__objects[key]
 
         # iterate through attr names and values
         for i, att_name in enumerate(args):
