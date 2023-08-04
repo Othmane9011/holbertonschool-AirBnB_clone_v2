3fd6d9993ce5e874b2f4933a39b34a2efbb8a9b3
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-08-04 20:50:46 +0200

    update city and state

--- models/state.py
+++ models/state.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models import storage
-from models.city import City
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship(
-        "City",
-        backref='state',
-        cascade="all, delete",
-        passive_deletes=True)
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    __tablename__ = "states"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='states')
+    else:
         @property
         def cities(self):
-            """ Getter instance method """
-            all_cities = storage.all(City)
-            city_list = []
-
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-
-            return city_list
+            """ Getter attribute cities """
+            from models import storage
+            from models.city import City
+            list = []
+            cities = storage.all(City)
+            for value in cities.values():
+                if value.state_id == self.id:
+                    list.append(value)
+            return list
