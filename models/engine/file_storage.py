f80d4cd3b7ce0e02d8fe34f905a3e6e8ea7bcd0d
Othmane9011 <5650@holbertonstudents.com>
2023-08-18 21:46:09 +0200

    task7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 class FileStorage:
@@ -16,14 +9,16 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
-        if cls is not None:
-            new_dict = {}
-            for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
-            return new_dict
-        return self.__objects
+        """Returns a dictionary of models currently in storage"""
+        if cls is None:
+            return FileStorage.__objects
+        new_dict = {}
+        name_cls = cls.__name__
+        key = FileStorage.__objects.keys()
+        for i in key:
+            if name_cls == i.split(".")[0]:
+                new_dict[i] = FileStorage.__objects[i]
+        return new_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -36,15 +31,22 @@ class FileStorage:
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
-            json.dump(temp, f)
+            json.dump(temp, f, indent=2)
 
     def reload(self):
         """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -55,13 +57,11 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Method that deletes obj from __objects"""
+        """function that deletes an object"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects:
-                del self.__objects[key]
+            key = obj.__class__.__name__ + "." + obj.id
+            del (FileStorage.__objects[key])
 
     def close(self):
-        """
-        """
+        """ Function that call the reload method """
         self.reload()
--- models/state.py
+++ models/state.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
+    """This is the class for state"""
+
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    cities = relationship('City', backref='state')
 
     def __init__(self, *args, **kwargs):
-        """
-        init inherited
-        """
+        """Inicialization inherited """
         super().__init__(*args, **kwargs)
 
-    if models.storage_type != "db":
+    if models.storage_type != 'db':
         @property
         def cities(self):
-            """getter for cities that return
-            a list of city instance equale to
-            curent state id
-            """
-            list_city = []
-            all_inst_c = models.storage.all(City)
-            for value in all_inst_c.values():
+            """Gets the list of City instances with state_id=State.id"""
+            city_list = []
+            all_city = models.storage.all(City)
+            for value in all_city.values():
                 if value.state_id == self.id:
-                    list_city.append(value)
-            return list_city
+                    city_list.append(value)
+            return city_list
